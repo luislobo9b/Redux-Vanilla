084573327a7bf82b53d2cafcd38c5972630e8d8e
Luis Lobo <luis.lobo9b@gmail.com>
2024-10-28 21:52:54 -0300

    criado todos os botões e elementos na interface para permitir o uso do store pelo usuário e selecionado os mesmos no app.mjs

--- js/app.mjs
+++ js/app.mjs
@@ -1,1 +1,8 @@
 import { store, getCounter, setCounter, decrementCounter, incrementCounter } from "./store.mjs"
+
+const btnGetCounter = document.querySelector("#getCounter"),
+	btnSetCounter = document.querySelector("#setCounter"),
+	inputCounter = document.querySelector("#inputCounter"),
+	btnDecrementCounter = document.querySelector("#decrementCounter"),
+	btnIncrementCounter = document.querySelector("#incrementCounter"),
+	resultElement = document.querySelector("#result")
